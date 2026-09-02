a95d1464256c4b95331a615c733d526518dc03e7
kirakiralingo-1 <kirakiralingo@outlook.jp>
2026-09-02 11:33:54 +0900

    Delete game/hardcore-ninja-main directory

--- game/hardcore-ninja-main/src/themes/base/components/Button/Button.ts
+++
@@ -1,9 +0,0 @@
-import type { MantineThemeComponent } from '@mantine/core';
-import classes from './Button.module.css';
-
-const Button: MantineThemeComponent = {
-  defaultProps: {},
-  classNames: classes,
-};
-
-export default Button;
